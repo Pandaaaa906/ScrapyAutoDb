01ac4662248904b9b56ff0b38eadc6a58846238e
Pandaaaa906 <ye.pandaaaa906@gmail.com>
2017-06-29 23:20:56 +0800

    Add OrderedItem

--- scrapyautodb/item.py
+++ scrapyautodb/item.py
@@ -1,6 +1,8 @@
 from collections import OrderedDict
 from abc import ABCMeta
 import itertools
+
+import scrapy
 import six
 from scrapy.item import DictItem
 
@@ -13,25 +15,35 @@ class Field(dict):
         self.count = Field._counter.next()
 
 
-class ItemMeta(ABCMeta):
+class ItemMeta(scrapy.item.ItemMeta):
     def __new__(mcs, class_name, bases, attrs):
         new_bases = tuple(base._class for base in bases if hasattr(base, '_class'))
         _class = super(ItemMeta, mcs).__new__(mcs, 'x_' + class_name, new_bases, attrs)
 
-        fields = getattr(_class, 'fields', {})
+        tmp_fields = getattr(_class, 'fields', OrderedDict())
         new_attrs = {}
         for n in dir(_class):
             v = getattr(_class, n)
             if isinstance(v, Field):
-                fields[n] = v
+                tmp_fields[n] = v
             elif n in attrs:
                 new_attrs[n] = attrs[n]
 
-        fields = OrderedDict(sorted(fields.items(), key=lambda item: getattr(_class, item[0]).count))
+        sorted_keys = sorted(tmp_fields, key=lambda tkey: mcs._key(_class, tkey))
+        fields = OrderedDict()
+        for key in sorted_keys:
+            fields[key] = tmp_fields.pop(key)
 
         new_attrs['fields'] = fields
         new_attrs['_class'] = _class
         return super(ItemMeta, mcs).__new__(mcs, class_name, bases, new_attrs)
+
+    def _key(cls, attr):
+        field = getattr(cls, attr)
+        if hasattr(field,"count"):
+            return field.count
+        else:
+            raise AttributeError("Field Type is wrong")
 
 
 @six.add_metaclass(ItemMeta)
